a078ebfec6d6c68a89dcf8555b7dbb1fb977473d
calimad <calimanescuadrian@gmail.com>
2023-01-08 00:11:34 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+class User():
+
+    def __init__(self, name):
+        self.name = name
+
+    def buy(self):
+        pass
+
+
+class Seat():
+    pass
+
+
+class Card():
+    pass
+
+
+class Ticket():
+    pass
+
