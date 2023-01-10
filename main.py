060be6f2396a05feee89ee45d93078bb4d9407ed
calimad <calimanescuadrian@gmail.com>
2023-01-10 23:46:40 +0200

    Create all the methods except buy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import sqlite3
 import string
+from fpdf import FPDF
 
 
 class User:
@@ -20,17 +21,38 @@ class Seat:
     def __init__(self, seat_id):
         self.seat_id = seat_id
 
-    def price(self):
+    def get_price(self):
         """Get the price of the seat"""
-        pass
+        connection = sqlite3.connect("cinema.db")
+        cursor = connection.cursor()
+        cursor.execute("""
+        SELECT price FROM seat WHERE seat_id=?
+        """, [self.seat_id])
+        result = cursor.fetchall()[0][0]  # [0][0] so it returns the pure value form the tuple from the list
+        return result
 
     def is_free(self):
         """Check the database if it's taken or not"""
-        pass
+        connection = sqlite3.connect("cinema.db")
+        cursor = connection.cursor()
+        cursor.execute("""
+        SELECT taken FROM seat WHERE seat_id = ?
+        """, [self.seat_id])
+        result = cursor.fetchall()[0][0]
+
+        if result == 0:
+            return True
+        else:
+            return False
 
     def occupy(self):
         """Change the value of taken in database to 1 if seat is free"""
-        pass
+        connection = sqlite3.connect("cinema.db")
+        connection.execute("""
+        UPDATE Seat SET taken=? WHERE seat_id=?
+        """, [1, self.seat_id])
+        connection.commit()
+        connection.close()
 
 
 class Card:
@@ -43,23 +65,67 @@ class Card:
         self.cvc = cvc
         self.holder = holder
 
-    def validate(self):
+    def validate(self, price):
         """Check if the card is valid
         Subtract amount from balance"""
-        pass
+        connection = sqlite3.connect("banking.db")
+        cursor = connection.cursor()
+        cursor.execute("""
+        SELECT balance FROM card WHERE type=? number=? and cvc=? and holder=?
+        """, [self.type, self.number, self.cvc, self.holder])
+        result = cursor.fetchall()
 
+        if result:
+            balance = result[0][0]
+            if balance >= result:
+                connection.execute("""
+                UPDATE Card SET balance = ? WHERE type=? and number=? cvc=? and holder=?
+                """, [balance-price, self.type, self.number, self.cvc, self.holder])
+                connection.commit()
+                connection.close()
+                return True
 
 class Ticket:
     """Represents the digital ticket"""
-    def __init__(self, user, price, seat):  # generate a random id using random import
+    def __init__(self, user, price, seat_nr):  # generate a random id using random import
         self.id = "".join([random.choice(string.ascii_letters) for i in range(8)])
         self.user = user
         self.price = price
-        self.seat = seat
+        self.seat_nr = seat_nr
 
     def to_pdf(self):
         """Generate the PDF"""
-        pass
+        pdf = FPDF(orientation="P", unit="pt", format="A4")
+        pdf.add_page()
+
+        pdf.set_font(family="Times", style="B", size=24)
+        pdf.cell(w=0, h=80, txt="Your Digital Ticket", border=1, ln=1, align="C")
+
+        pdf.set_font(family="Times", style="B", size=14)
+        pdf.cell(w=100, h=25, txt="Name: ", border=1)
+        pdf.set_font(family="Times", style="B", size=12)
+        pdf.cell(w=0, h=25, txt=self.user.name, border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=0, ln=1)
+
+        pdf.set_font(family="Times", style="B", size=14)
+        pdf.cell(w=100, h=25, txt="Ticket ID: ", border=1)
+        pdf.set_font(family="Times", style="B", size=12)
+        pdf.cell(w=0, h=25, txt=self.id, border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=0, ln=1)
+
+        pdf.set_font(family="Times", style="B", size=14)
+        pdf.cell(w=100, h=25, txt="Price: ", border=1)
+        pdf.set_font(family="Times", style="B", size=12)
+        pdf.cell(w=0, h=25, txt=self.price, border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=0, ln=1)
+
+        pdf.set_font(family="Times", style="B", size=14)
+        pdf.cell(w=100, h=25, txt="Price: ", border=1)
+        pdf.set_font(family="Times", style="B", size=12)
+        pdf.cell(w=0, h=25, txt=self.seat_nr, border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=0, ln=1)
+
+        pdf.output("Ticket.pdf", "F")
 
 
 # The command prompt
